73d0003264648c40cf7e2bb7044c6907eb0a62e8
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-01 13:54:53 -0400

    sumItems handles no nested arrays

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const sumItems = (array) => {
   if (array.length === 1) {
     return array[0];
   }
-};
 
+  while (array.length > 1) {
+    array[0] += array[array.length - 1];
+    array.pop();
+  }
+  return array[0];
+};
 module.exports = sumItems;
