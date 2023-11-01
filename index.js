0e6d4f8000ea39976991cf3ef1808905c65ac9b1
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-01 13:21:39 -0400

    add base case to sumItems

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+// Sum all the numbers in the array
+const sumItems = (array) => {
+  if (array.length === 1) {
+    return array[0];
+  }
+};
 
 module.exports = sumItems;
